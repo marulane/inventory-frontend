33b4d17ed22d46ef82b00de81a09321f4ebec175
Luisa Montes <luisa.montesrmz@gmail.com>
2024-06-03 19:43:57 -0500

    mejora en paginador

--- src/app/modules/category/components/category/category.component.ts
+++ src/app/modules/category/components/category/category.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, ViewChild, inject } from '@angular/core';
 import { CategoryService } from '../../../shared/services/category.service';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatDialog } from '@angular/material/dialog';
 import { NewCategoryComponent } from '../new-category/new-category.component';
 import { MatSnackBar, MatSnackBarRef, SimpleSnackBar } from '@angular/material/snack-bar';
 import { ConfirmComponent } from '../../../shared/components/confirm/confirm.component';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-category',
@@ -26,7 +27,9 @@ export class CategoryComponent implements OnInit{ //OnInit ahora se implementa m
 
   displayedColumns: string[]=['id', 'name', 'description', 'actions'];
   dataSource = new MatTableDataSource<CategoryElement>();
-
+  
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator
   //* OBTENER TODAS LAS CATEGORÍAS */
   getCategories():void{
     this.categoryService.getCategories()
@@ -51,6 +54,7 @@ export class CategoryComponent implements OnInit{ //OnInit ahora se implementa m
       });
 
       this.dataSource = new MatTableDataSource<CategoryElement>(dataCategory);
+      this.dataSource.paginator = this.paginator;
     }
   }
 
